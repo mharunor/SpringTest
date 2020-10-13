ca1d772a963049765ab4fb4e26e1f7261675e92e
Mohammad Rashid <harunor.md@gmail.com>
2020-10-13 11:58:44 -0400

    3rd commit

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -27,6 +27,7 @@ public class DemoApplication {
 			toDoRepository.save(new ToDo("Clean the code", true));
 			toDoRepository.save(new ToDo("Build the artifacts", false));
 			toDoRepository.save(new ToDo("Deploy the jar file", true));
+			toDoRepository.save(new ToDo("Deploy", true));
 			logger.info("The sample data has been generated");
 		};
 	}
